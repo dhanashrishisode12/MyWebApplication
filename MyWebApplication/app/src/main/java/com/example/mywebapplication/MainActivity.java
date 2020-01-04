cfa48f4f76c2314e81c9f1100a9c78dd707c0319
Pavan Shisode <pavansisode@gmail.com>
2020-01-05 00:24:34 +0530

    my first commit

---
+++ MyWebApplication/app/src/main/java/com/example/mywebapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mywebapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        //webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://dhanashrishisode.orgfree.com/seals/index.php");
+       // WebSettings webSettings = webView.getSettings();
+        //webSettings.setJavaScriptEnabled(true);
+    }
+}
