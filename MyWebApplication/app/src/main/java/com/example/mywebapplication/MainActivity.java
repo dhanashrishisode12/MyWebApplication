128d13ac01d06239d080d63dba4a91c10f33bf22
Pavan Shisode <pavansisode@gmail.com>
2020-01-05 00:48:56 +0530

    Uncomment the code.added unversioned file.
    Signed-off-by: Pavan Shisode <pavansisode@gmail.com>

--- MyWebApplication/app/src/main/java/com/example/mywebapplication/MainActivity.java
+++ MyWebApplication/app/src/main/java/com/example/mywebapplication/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = (WebView)findViewById(R.id.webView);
-        //webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://dhanashrishisode.orgfree.com/seals/index.php");
        // WebSettings webSettings = webView.getSettings();
         //webSettings.setJavaScriptEnabled(true);
